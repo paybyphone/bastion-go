55f775cbf7d5d1296a19c084027bc29f4a305486
Chris Marchesi <cmarchesi@paybyphone.com>
2016-08-19 17:42:21 -0700

    Instance support and test scaffolds
    Instance stuff done, just working on mocking out all the stuff needed to
    do testing on the code then this part will be good to go.

--- aws/instance.go
+++ aws/instance.go
@@ -2,12 +2,18 @@ package aws
 
 import (
 	"fmt"
+	"log"
 	"sort"
 	"time"
+
+	"golang.org/x/crypto/ssh"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
+
+// The instance start timeout, in seconds.
+const startTimeout = 300
 
 // The instance type to launch.
 const instanceType = "t2.nano"
@@ -44,6 +50,12 @@ func amiSearchParameters() *ec2.DescribeImagesInput {
 // Instance describes an AWS EC2 instance.
 type Instance struct {
 	_ struct{}
+
+	// true if the instance has been created.
+	Created bool `json:"created"`
+
+	// The ID of the AMI used to launch the instance.
+	ImageID string `json:"image_id"`
 
 	// The ID of the instance.
 	InstanceID string `json:"instance_id"`
@@ -93,6 +105,77 @@ func mostRecentAmi(images []*ec2.Image) *ec2.Image {
 	return sortedImages[len(sortedImages)-1]
 }
 
+// waitForInstanceStart waits for the instance to start, and returns the
+// properly updated *ec2.Instance object.
+func waitForInstanceStart(conn *ec2.EC2, instanceID string, timeout int) (*ec2.Instance, error) {
+	params := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			&ec2.Filter{
+				Name:   aws.String("instance-id"),
+				Values: aws.StringSlice([]string{instanceID}),
+			},
+		},
+	}
+
+	start := time.Now()
+	d := time.Duration(timeout) * time.Second
+	max := start.Add(d)
+
+	for time.Now().After(max) == false {
+		resp, err := conn.DescribeInstances(params)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(resp.Reservations) != 1 {
+			panic(fmt.Errorf("Expected a single reservation, got %d", len(resp.Reservations)))
+		}
+
+		if len(resp.Reservations[0].Instances) < 1 {
+			return nil, fmt.Errorf("No instances were found.")
+		}
+
+		if len(resp.Reservations[0].Instances) > 1 {
+			panic("More than one instance was found when only one was requested")
+		}
+
+		instance := resp.Reservations[0].Instances[0]
+		if *instance.State.Name == "running" {
+			return instance, nil
+		}
+	}
+
+	return nil, fmt.Errorf("Instance was not started after %d seconds", timeout)
+}
+
+// waitForSSH waits not only for SSH to be running and open, but also ensures
+// that the IP address can be reached via the configured SSH user.
+func waitForSSH(addr, user string, key KeyPair, timeout int) error {
+	signer, err := ssh.ParsePrivateKey([]byte(key.PrivateKeyPEM))
+	if err != nil {
+		log.Fatalf("Unable to parse private key: %s", err.Error())
+	}
+
+	config := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+	}
+	start := time.Now()
+	d := time.Duration(timeout) * time.Second
+	max := start.Add(d)
+
+	for time.Now().After(max) == false {
+		_, err := ssh.Dial("tcp", addr, config)
+		if err == nil {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("SSH could not be connected after %d seconds", timeout)
+}
+
 // LocateImage searches for a suitable AMI to launch, based off the
 // filters supplied by amiSearchParameters().
 func LocateImage(conn *ec2.EC2) (string, error) {
@@ -111,4 +194,91 @@ func LocateImage(conn *ec2.EC2) (string, error) {
 	image := mostRecentAmi(resp.Images)
 
 	return *image.ImageId, nil
+}
+
+// CreateInstance creates an Amazon EC2 insatnce, and returns an Instance
+// struct.
+func CreateInstance(conn *ec2.EC2, subnet, securityGroup string, keyPair KeyPair) (Instance, error) {
+	instance := Instance{
+		SubnetID:        subnet,
+		KeyPairName:     keyPair.KeyName,
+		SecurityGroupID: securityGroup,
+		InstanceType:    instanceType,
+		SSHUser:         sshUser,
+	}
+	// Locate an AMI for the instance
+	ami, err := LocateImage(conn)
+	if err != nil {
+		return instance, err
+	}
+
+	// Attempt to launch the instance.
+	params := &ec2.RunInstancesInput{
+		ImageId:      aws.String(ami),
+		InstanceType: aws.String(instanceType),
+		KeyName:      aws.String(keyPair.KeyName),
+		MaxCount:     aws.Int64(1),
+		MinCount:     aws.Int64(1),
+		NetworkInterfaces: []*ec2.InstanceNetworkInterfaceSpecification{
+			&ec2.InstanceNetworkInterfaceSpecification{
+				AssociatePublicIpAddress: aws.Bool(true),
+				DeleteOnTermination:      aws.Bool(true),
+				DeviceIndex:              aws.Int64(0),
+				Groups:                   aws.StringSlice([]string{securityGroup}),
+				SubnetId:                 aws.String(subnet),
+			},
+		},
+	}
+
+	resp, err := conn.RunInstances(params)
+	if err != nil {
+		return instance, err
+	}
+
+	if len(resp.Instances) < 1 {
+		return instance, fmt.Errorf("No instances were launched.")
+	}
+
+	if len(resp.Instances) > 1 {
+		panic("More than one instance was launched when only one was requested")
+	}
+
+	// Wait for the instance to be started.
+	newInstance, err := waitForInstanceStart(conn, *resp.Instances[0].InstanceId, startTimeout)
+	if err != nil {
+		return instance, err
+	}
+
+	// Wait for SSH off the new instance public IP address
+	if newInstance.PublicIpAddress == nil {
+		return instance, fmt.Errorf("Instance ID %s does not have a public IP address.", *newInstance.InstanceId)
+	}
+
+	err = waitForSSH(*newInstance.PublicIpAddress, sshUser, keyPair, startTimeout)
+	if err != nil {
+		return instance, err
+	}
+
+	// Done
+	instance.InstanceID = *newInstance.InstanceId
+	instance.PublicIPAddress = *newInstance.PublicIpAddress
+	instance.PrivateIPAddress = *newInstance.PrivateIpAddress
+	instance.Created = true
+
+	return instance, nil
+}
+
+// DeleteInstance terminates an Amazon EC2 instance.
+func DeleteInstance(conn *ec2.EC2, instance Instance) (Instance, error) {
+	params := &ec2.TerminateInstancesInput{
+		InstanceIds: aws.StringSlice([]string{instance.InstanceID}),
+	}
+
+	_, err := conn.TerminateInstances(params)
+	if err != nil {
+		return instance, err
+	}
+
+	instance.Created = false
+	return instance, nil
 }
--- aws/instance_test.go
+++ aws/instance_test.go
@@ -1,7 +1,11 @@
 package aws
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
@@ -12,6 +16,36 @@ import (
 func testDescribeImagesOutput() *ec2.DescribeImagesOutput {
 	return &ec2.DescribeImagesOutput{
 		Images: []*ec2.Image{
+			&ec2.Image{
+				Architecture: aws.String("x86_64"),
+				BlockDeviceMappings: []*ec2.BlockDeviceMapping{
+					&ec2.BlockDeviceMapping{
+						DeviceName: aws.String("/dev/xvda"),
+						Ebs: &ec2.EbsBlockDevice{
+							DeleteOnTermination: aws.Bool(true),
+							Encrypted:           aws.Bool(false),
+							SnapshotId:          aws.String("snap-6d465049"),
+							VolumeSize:          aws.Int64(8),
+							VolumeType:          aws.String("gp2"),
+						},
+					},
+				},
+				CreationDate:       aws.String("2014-06-22T09:19:44.000Z"),
+				Description:        aws.String("Amazon Linux AMI 2014.03.3 x86_64 HVM GP2"),
+				Hypervisor:         aws.String("xen"),
+				ImageId:            aws.String("ami-8172b616"),
+				ImageLocation:      aws.String("amazon/amzn-ami-hvm-2014.03.3.x86_64-gp2"),
+				ImageOwnerAlias:    aws.String("amazon"),
+				ImageType:          aws.String("machine"),
+				Name:               aws.String("amzn-ami-hvm-2014.03.3.x86_64-gp2"),
+				OwnerId:            aws.String("137112412989"),
+				Public:             aws.Bool(true),
+				RootDeviceName:     aws.String("/dev/xvda"),
+				RootDeviceType:     aws.String("ebs"),
+				SriovNetSupport:    aws.String("simple"),
+				State:              aws.String("available"),
+				VirtualizationType: aws.String("hvm"),
+			},
 			&ec2.Image{
 				Architecture: aws.String("x86_64"),
 				BlockDeviceMappings: []*ec2.BlockDeviceMapping{
@@ -42,6 +76,157 @@ func testDescribeImagesOutput() *ec2.DescribeImagesOutput {
 				State:              aws.String("available"),
 				VirtualizationType: aws.String("hvm"),
 			},
+			&ec2.Image{
+				Architecture: aws.String("x86_64"),
+				BlockDeviceMappings: []*ec2.BlockDeviceMapping{
+					&ec2.BlockDeviceMapping{
+						DeviceName: aws.String("/dev/xvda"),
+						Ebs: &ec2.EbsBlockDevice{
+							DeleteOnTermination: aws.Bool(true),
+							Encrypted:           aws.Bool(false),
+							SnapshotId:          aws.String("snap-5d465048"),
+							VolumeSize:          aws.Int64(8),
+							VolumeType:          aws.String("gp2"),
+						},
+					},
+				},
+				CreationDate:       aws.String("2015-06-22T09:19:44.000Z"),
+				Description:        aws.String("Amazon Linux AMI 2015.03.3 x86_64 HVM GP2"),
+				Hypervisor:         aws.String("xen"),
+				ImageId:            aws.String("ami-7172b612"),
+				ImageLocation:      aws.String("amazon/amzn-ami-hvm-2015.03.3.x86_64-gp2"),
+				ImageOwnerAlias:    aws.String("amazon"),
+				ImageType:          aws.String("machine"),
+				Name:               aws.String("amzn-ami-hvm-2015.03.3.x86_64-gp2"),
+				OwnerId:            aws.String("137112412989"),
+				Public:             aws.Bool(true),
+				RootDeviceName:     aws.String("/dev/xvda"),
+				RootDeviceType:     aws.String("ebs"),
+				SriovNetSupport:    aws.String("simple"),
+				State:              aws.String("available"),
+				VirtualizationType: aws.String("hvm"),
+			},
 		},
 	}
+}
+
+// testEC2Reservation provides a test ec2.Reservation struct.
+//
+// This type is used in the ec2.DescribeInstances() and ec2.RunInstances()
+// functions.
+func testEC2Reservation() *ec2.Reservation {
+	return &ec2.Reservation{
+		Instances: []*ec2.Instance{
+			&ec2.Instance{
+				State: &ec2.InstanceState{
+					Code: aws.Int64(16),
+					Name: aws.String("running"),
+				},
+				InstanceId:       aws.String("i-1234567890abcdef0"),
+				PrivateIpAddress: aws.String("10.0.0.1"),
+				PublicIpAddress:  aws.String("54.0.0.1"),
+			},
+		},
+	}
+}
+
+// testDescribeInstancesOutput provides a test ec2.DescribeInstancesOutput
+// object.
+func testDescribeInstancesOutput() *ec2.DescribeInstancesOutput {
+	return &ec2.DescribeInstancesOutput{
+		Reservations: []*ec2.Reservation{
+			testEC2Reservation(),
+		},
+	}
+}
+
+// testInstance provides a test Instance struct.
+func testInstance() Instance {
+	return Instance{
+		Created:          true,
+		ImageID:          "ami-7172b611",
+		InstanceID:       "i-1234567890abcdef0",
+		InstanceType:     "t2.nano",
+		SubnetID:         "subnet-1234567890abcdef0",
+		KeyPairName:      "bastion-test",
+		SecurityGroupID:  "sg-1234567890abcdef0",
+		PublicIPAddress:  "8.8.8.8",
+		PrivateIPAddress: "10.0.0.1",
+		SSHUser:          "ec2-user",
+	}
+}
+
+// testDescribeImages is a stub function for testing the
+// ec2.DescribeImages function.
+func testDescribeImages(input *ec2.DescribeImagesInput) (*ec2.DescribeImagesOutput, error) {
+	if *input.ImageIds[0] == "bad" {
+		return nil, fmt.Errorf("error")
+	}
+	return testDescribeImagesOutput(), nil
+}
+
+// testDescribeInstances is a stub function for testing the
+// ec2.DescribeInstances function.
+func testDescribeInstances(input *ec2.DescribeInstancesInput) (*ec2.DescribeInstancesOutput, error) {
+	if *input.InstanceIds[0] == "bad" {
+		return nil, fmt.Errorf("error")
+	}
+	return testDescribeInstancesOutput(), nil
+}
+
+// testRunInstances is a stub function for testing the
+// ec2.RunInstances function.
+func testRunInstances(input *ec2.RunInstancesInput) (*ec2.Reservation, error) {
+	if *input.PrivateIpAddress == "bad" {
+		return nil, fmt.Errorf("error")
+	}
+	return testEC2Reservation(), nil
+}
+
+// testTerminateInstances is a stub function for testing the
+// ec2.TerminateInstances function.
+func testTerminateInstances(input *ec2.TerminateInstancesInput) (*ec2.TerminateInstancesOutput, error) {
+	if *input.InstanceIds[0] == "bad" {
+		return nil, fmt.Errorf("error")
+	}
+	return &ec2.TerminateInstancesOutput{}, nil
+}
+
+// createTestEC2InstanceMock returns a mock EC2 service to use with the
+// instance test functions.
+func createTestEC2InstanceMock() *ec2.EC2 {
+	conn := ec2.New(session.New(), nil)
+	conn.Handlers.Clear()
+
+	conn.Handlers.Send.PushBack(func(r *request.Request) {
+		switch p := r.Params.(type) {
+		case *ec2.DescribeImagesInput:
+			out, err := testDescribeImages(p)
+			if out != nil {
+				*r.Data.(*ec2.DescribeImagesOutput) = *out
+			}
+			r.Error = err
+		case *ec2.DescribeInstancesInput:
+			out, err := testDescribeInstances(p)
+			if out != nil {
+				*r.Data.(*ec2.DescribeInstancesOutput) = *out
+			}
+			r.Error = err
+		case *ec2.RunInstancesInput:
+			out, err := testRunInstances(p)
+			if out != nil {
+				*r.Data.(*ec2.Reservation) = *out
+			}
+			r.Error = err
+		case *ec2.TerminateInstancesInput:
+			out, err := testTerminateInstances(p)
+			if out != nil {
+				*r.Data.(*ec2.TerminateInstancesOutput) = *out
+			}
+			r.Error = err
+		default:
+			panic(fmt.Errorf("Unsupported input type %T", p))
+		}
+	})
+	return conn
 }
